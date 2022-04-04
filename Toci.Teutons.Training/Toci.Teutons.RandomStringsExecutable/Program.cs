398af48e7946ba4996fa4fb9896170dac8b9f9cf
Przemek Wołczacki <61731807+wolczi@users.noreply.github.com>
2022-04-04 19:10:45 +0200

    Generator Przemek

---
+++ Toci.Teutons.Training/Toci.Teutons.RandomStringsExecutable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Teutons.RandomStringsExecutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrzemekGeneratorExtension generator = new PrzemekGeneratorExtension();
+
+            Console.WriteLine(generator.GenerateString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Toci.Teutons.Training/Toci.Teutons.RandomStringsExecutable/PrzemekGeneratorExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Teutons.RandomStringsExecutable
+{
+    public class PrzemekGeneratorExtension : PrzemekGeneratorBase
+    {
+        protected int passwordLength;
+        Random r = new Random();
+
+        protected override void GetRandomString(int minLength, int maxLength, ref StringBuilder sbResult)
+        {
+            passwordLength = r.Next(minLength, maxLength);
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                sbResult.Append(r.Next(0, 2) == 0 ? Convert.ToChar(r.Next(65, 90)) : Convert.ToChar(r.Next(97, 122)));
+            }
+        }
+
+        protected override void GetRandomPassword(ref StringBuilder sbResult)
+        {
+            passwordLength = r.Next(8, 40);
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                sbResult.Append(r.Next(0, 2) == 0 ? Convert.ToChar(r.Next(65, 90)) : Convert.ToChar(r.Next(97, 122)));
+            }
+        }
+
+
+        protected override void GetStrongRandomPassword(ref StringBuilder sbResult)
+        {
+            passwordLength = r.Next(8, 40);
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                sbResult.Append(Convert.ToChar(r.Next(33, 126)));
+            }
+        }
+    }
+}
