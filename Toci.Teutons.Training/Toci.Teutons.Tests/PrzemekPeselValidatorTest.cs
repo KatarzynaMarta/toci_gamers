c2782a40884bf78d8ec00cea113b22529b4b22b7
Przemek Wołczacki <61731807+wolczi@users.noreply.github.com>
2022-04-07 22:07:42 +0200

    przemek pesel validator with tests

---
+++ Toci.Teutons.Training/Toci.Teutons.Tests/PrzemekPeselValidatorTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Teutons.Training.Pesel;
+
+namespace Toci.Teutons.Tests
+{
+    [TestClass]
+    public class PrzemekPeselValidatorTest
+    {
+        class PeselEntity
+        {
+            public string Pesel { get; set; }
+            public bool Result { get; set; }
+        }
+
+        [TestMethod]
+        public void PeselTest()
+        {
+            
+            PrzemekPeselValidator przemekPeselValidator = new PrzemekPeselValidator();
+
+            List<PeselEntity> testCases = new List<PeselEntity>()
+            {
+                new PeselEntity() { Pesel = "98092102294", Result = true },
+                new PeselEntity() { Pesel = "98092102293", Result = false },
+            };
+
+            foreach (PeselEntity item in testCases)
+            {
+                Assert.AreEqual(item.Result, przemekPeselValidator.IsPeselValid(item.Pesel));
+            }
+        }
+
+    }
+}
+
+
---
+++ Toci.Teutons.Training/Toci.Teutons.Training/Pesel/PrzemekPeselValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Teutons.Training.Pesel
+{
+    public class PrzemekPeselValidator : PeselValidatorBase
+    {
+
+
+        public override bool IsPeselValid(string peselCandidate)
+        {
+            int[] multipliers = new int[] { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int counter = 0;
+            int number;
+
+            int peselLastNumber = (int)Char.GetNumericValue(peselCandidate[peselCandidate.Length - 1]);
+
+            for(int i= 0; i < peselCandidate.Length-1; i++)
+            {
+                number = (int)Char.GetNumericValue(peselCandidate[i]);
+                counter += number * multipliers[i];
+            }
+
+            int result = 10 - (counter % 10);
+
+            if (result == peselLastNumber) return true;
+            else return false;
+        }
+    }
+}
